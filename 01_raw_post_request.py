5113b87c23ccfa03f99531cc922ad3a5ddc2da0f
Craig West <iwswordpress@gmail.com>
2025-10-18 14:27:10 +0100

    Clear Git cache

---
+++ 01_raw_post_request.py
@@ -0,0 +1,42 @@
+import os
+import json
+from dotenv import load_dotenv, find_dotenv
+import requests
+from rich.console import Console
+
+console = Console()
+load_dotenv(find_dotenv(), override=True)  # read local .env file
+# get API key from .env file
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# PROMPTS
+
+system_prompt = "You are a helpfgul assistant."
+user_prompt = "Tell me about Brighton in 100 words or so."
+
+
+MODEL = "gpt-4o-mini"
+# just one endpoint and does not change based on request
+model_endpoint = "https://api.openai.com/v1/chat/completions"
+headers = {
+    "Content-Type": "application/json",  # Authorisation
+    "Authorization": f"Bearer {OPENAI_API_KEY}",
+}
+# payload structure may vary from LLM Organisation but it is a text string.
+# OpeAI trained models on the messages structure for chat models
+payload = {
+    "model": MODEL,
+    "messages": [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt},
+    ],
+}
+# API REQUEST
+# Use HTTP POST method - a web form in essence
+response = requests.post(
+    url=model_endpoint,  # The endpoint we are sending the request to.
+    headers=headers,  # Headers for authentication etc
+    data=json.dumps(payload),  # Inputs, Context, Instructions, Additonal parameters
+).json()
+
+console.print(response)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from brighton-web-development-meetup-oct-2025!")
+
+
+if __name__ == "__main__":
+    main()
