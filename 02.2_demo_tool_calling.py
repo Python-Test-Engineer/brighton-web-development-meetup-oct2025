c77d015ce91d6ce40f9c3adf487c4d7248302b52
Craig West <iwswordpress@gmail.com>
2025-10-19 10:28:16 +0100

    test

--- 02.2_demo_tool_calling.py
+++ 02.2_demo_tool_calling.py
@@ -5,6 +5,51 @@ from rich.console import Console
 
 console = Console()
 
+# SYSTEM PROMPT
+
+system_prompt = """
+You run in a loop of REQUEST_MORE_INFO, ACTION, UPDATE.
+
+You have a REQUEST_MORE_INFO of what you need to do, then you take an ACTION using tools provided, then you get an UPDATE. YOu keep repeating this until you have an ANSWER. Then you return the ANSWER and break out of the loop.
+
+You have two tools available for your ACTIONS - **calculate_total** and **get_product_price** so that you can get the total price of an item requested by the user.
+
+# 1. calculate_total:
+
+if amount = 200
+then calculate_total(amount)
+return amount * 1.2
+
+Runs the calculate_total function and returns a JSON FORMAT output as follows:
+{"result": 240, "fn": "calculate_total", "next": "PAUSE"}
+
+# 2. get_product_price:
+
+This uses the get_product_price function and passes in the value of the product
+e.g. get_product_price('bike')
+
+This uses the get_product_price with a product = 'bike', finds the price of the bike and then returns a JSON FORMAT output as follows:
+{"result": 200, "fn": "get_product_price", "next": "PAUSE"}
+
+ # Here is an example session:
+
+User Question: What is total cost of a bike including VAT?
+
+AI Response: REQUEST_MORE_INFO: I need to find the cost of a bike|ACTION|get_product_price|bike
+
+You will be called again with the result of get_product_price as the UPDATE and will have UPDATE|200 sent as another LLM prompt along with previous messages.
+
+Then the next message will be:
+
+REQUEST_MORE_INFO: I need to calculate the total including the VAT|ACTION|calculate_total|200
+
+The result wil be passed as another prompt as UPDATE|240 along with previous messages.
+
+If you have the ANSWER, output it as the ANSWER in this format:
+
+ANSWER|The price of the bike including VAT is 240
+
+"""
 
 # Load environment variables in a file called .env
 # Print the key prefixes to help with any debugging
@@ -52,6 +97,7 @@ else:
 print(f"LLM_CHOICE: {LLM_CHOICE} - MODEL: {MODEL}")
 # See card in ipynb version...
 
+
 # THIS IS A TOOL FUNCTION
 def get_product_price(product):
     if product == "bike":
@@ -61,6 +107,7 @@ def get_product_price(product):
     if product == "laptop":
         return 300
     return None
+
 
 # THIS IS A TOOL FUNCTION
 def calculate_total(amount):
@@ -90,51 +137,6 @@ class Agent:
         """Executes the ageLLM request and returns the result"""
         completion = client.chat.completions.create(model=MODEL, messages=self.messages)
         return completion.choices[0].message.content
-
-
-system_prompt = """
-You run in a loop of REQUEST_MORE_INFO, ACTION, UPDATE.
-
-You have a REQUEST_MORE_INFO of what you need to do, then you take an ACTION using tools provided, then you get an UPDATE. YOu keep repeating this until you have an ANSWER. Then you return the ANSWER and break out of the loop.
-
-You have two tools available for your ACTIONS - **calculate_total** and **get_product_price** so that you can get the total price of an item requested by the user.
-
-# 1. calculate_total:
-
-if amount = 200
-then calculate_total(amount)
-return amount * 1.2
-
-Runs the calculate_total function and returns a JSON FORMAT output as follows:
-{"result": 240, "fn": "calculate_total", "next": "PAUSE"}
-
-# 2. get_product_price:
-
-This uses the get_product_price function and passes in the value of the product
-e.g. get_product_price('bike')
-
-This uses the get_product_price with a product = 'bike', finds the price of the bike and then returns a JSON FORMAT output as follows:
-{"result": 200, "fn": "get_product_price", "next": "PAUSE"}
-
- # Here is an example session:
-
-User Question: What is total cost of a bike including VAT?
-
-AI Response: REQUEST_MORE_INFO: I need to find the cost of a bike|ACTION|get_product_price|bike
-
-You will be called again with the result of get_product_price as the UPDATE and will have UPDATE|200 sent as another LLM prompt along with previous messages.
-
-Then the next message will be:
-
-REQUEST_MORE_INFO: I need to calculate the total including the VAT|ACTION|calculate_total|200
-
-The result wil be passed as another prompt as UPDATE|240 along with previous messages.
-
-If you have the ANSWER, output it as the ANSWER in this format:
-
-ANSWER|The price of the bike including VAT is 240
-
-"""
 
 
 # A bit like recursion, we are going to call the agent in a loop until we get an answer.
